9c0bdcbb9217e295d471c0116af0d0a07aa790ff
None <None>
2017-07-18 18:43:53 -0700

    Minor tests

--- final-topo/run.py
+++ final-topo/run.py
@@ -30,19 +30,34 @@ class SimpleCPS(MiniCPS):
 
         self.net.start()
 
-	plc1 = self.net.get('plc101')
-	plc2 = self.net.get('plc201')
-	plc3 = self.net.get('plc301')
-
+	plc101 = self.net.get('plc101')
+	plc201 = self.net.get('plc201')
+	plc301 = self.net.get('plc301')
 
 	ids = self.net.get('ids101')
-	_intf = Intf( 'eth0', node=ids )
-	ids.cmd('ifconfig eth0 192.168.56.155')
+	_intf = Intf( 'eth2', node=ids )
+	ids.cmd('ifconfig eth2 192.168.56.101')
+
+	mv101 = self.net.get('mv101')
+	p101 = self.net.get('p101')
+	lit101 = self.net.get('lit101')
+	plant101 = self.net.get('plant101')
+	
+	p201 = self.net.get('p201')
+	ph201 = self.net.get('ph201')
+
+	lit301 = self.net.get('lit301')
+	p301 = self.net.get('p301')
+
+	#plc2.cmd(sys.executable + ' plc2.py &')
 
 
-	plc1.cmd('route add default gw 192.168.1.254 plc101-eth0  ')
-	plc2.cmd('route add default gw 192.168.2.254 plc201-eth0  ')
-	plc3.cmd('route add default gw 192.168.3.254 plc301-eth0  ')	
+
+	plc101.cmd('route add default gw 192.168.1.254 plc101-eth0  ')
+	plc201.cmd('route add default gw 192.168.2.254 plc201-eth0  ')
+	plc301.cmd('route add default gw 192.168.3.254 plc301-eth0  ')	
+
+	
 
         # start devices
         CLI(self.net)
