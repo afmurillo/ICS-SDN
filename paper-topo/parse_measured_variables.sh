3daae123e906efff33a9a41ef8b533e0e7fc3d38
None <None>
2017-11-28 08:56:07 -0800

    Results

--- paper-topo/parse_measured_variables.sh
+++ paper-topo/parse_measured_variables.sh
@@ -102,3 +102,11 @@ cat gaussian_noise_experiments/def_0_2.txt | grep 'PH Level'| awk '{print $4}' >
 cat gaussian_noise_experiments/def_0_2.txt | grep 'Tank 1'| awk '{print $6}' > gaussian_noise_experiments/tank_1_def_0_2.txt
 cat gaussian_noise_experiments/def_0_2.txt | grep 'Tank 2'| awk '{print $6}' > gaussian_noise_experiments/tank_2_def_0_2.txt
 
+cat gaussian_noise_experiments/no_def_0_1.txt | grep 'PH Level'| awk '{print $4}' > gaussian_noise_experiments/ph_no_def_0_1.txt
+cat gaussian_noise_experiments/no_def_0_1.txt | grep 'Tank 1'| awk '{print $6}' > gaussian_noise_experiments/tank_1_no_def_0_1.txt
+cat gaussian_noise_experiments/no_def_0_1.txt | grep 'Tank 2'| awk '{print $6}' > gaussian_noise_experiments/tank_2_no_def_0_1.txt
+
+cat gaussian_noise_experiments/def_0_1.txt | grep 'PH Level'| awk '{print $4}' > gaussian_noise_experiments/ph_def_0_1.txt
+cat gaussian_noise_experiments/def_0_1.txt | grep 'Tank 1'| awk '{print $6}' > gaussian_noise_experiments/tank_1_def_0_1.txt
+cat gaussian_noise_experiments/def_0_1.txt | grep 'Tank 2'| awk '{print $6}' > gaussian_noise_experiments/tank_2_def_0_1.txt
+
