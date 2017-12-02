93a32fd5d7ea3cc52922b529236c72190148cb75
None <None>
2017-12-02 12:15:20 -0800

    Results

--- paper-topo/parse_measured_variables.sh
+++ paper-topo/parse_measured_variables.sh
@@ -115,9 +115,9 @@ cat second_gaussian/def_0_1.txt | grep 'PH Level'| awk '{print $4}' > second_gau
 cat second_gaussian/def_0_1.txt | grep 'Tank 1'| awk '{print $6}' > second_gaussian/tank_1_def_0_1.txt
 cat second_gaussian/def_0_1.txt | grep 'Tank 2'| awk '{print $6}' > second_gaussian/tank_2_def_0_1.txt
 
-cat second_gaussian/def_0_2.txt | grep 'PH Level'| awk '{print $4}' > second_gaussian/ph_def_0_2.txt
-cat second_gaussian/def_0_2.txt | grep 'Tank 1'| awk '{print $6}' > second_gaussian/tank_1_def_0_2.txt
-cat second_gaussian/def_0_2.txt | grep 'Tank 2'| awk '{print $6}' > second_gaussian/tank_2_def_0_2.txt
+cat second_gaussian/no_def_0_2.txt | grep 'PH Level'| awk '{print $4}' > second_gaussian/ph_no_def_0_2.txt
+cat second_gaussian/no_def_0_2.txt | grep 'Tank 1'| awk '{print $6}' > second_gaussian/tank_1_no_def_0_2.txt
+cat second_gaussian/no_def_0_2.txt | grep 'Tank 2'| awk '{print $6}' > second_gaussian/tank_2_no_def_0_2.txt
 
 cat second_gaussian/def_0_0_2.txt | grep 'PH Level'| awk '{print $4}' > second_gaussian/ph_def_0_0_2.txt
 cat second_gaussian/def_0_0_2.txt | grep 'Tank 1'| awk '{print $6}' > second_gaussian/tank_1_def_0_0_2.txt
