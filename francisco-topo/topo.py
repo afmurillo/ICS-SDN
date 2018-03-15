564337f874e806455d03d25a64d72832bdf0235c
andrecalderon <andrea_calderon@hotmail.com>
2018-03-14 21:57:30 -0500

    minor fixes on topo

--- francisco-topo/topo.py
+++ francisco-topo/topo.py
@@ -23,20 +23,16 @@ class SimpleTopo(Topo):
     def build(self):
 
         # Add router
-        #defaultIP = '192.168.1.254/24'  # IP address for r0-eth1
         defaultIP = IP['plc101']+NETMASK
-	#router = self.addNode( 'r0', cls=LinuxRouter, ip=defaultIP )
-        host = self.addHost('hmi', ip='192.168.4.4/24')
+
 
         # Add switches
-        s1, s2, s3, s4  = [ self.addSwitch( s ) for s in ('s1', 's2', 's3', 's4') ]
-        self.addLink(host, s4)
+        s1, s2, s3  = [ self.addSwitch( s ) for s in ('s1', 's2', 's3') ]
 
         gateway_1 = 'via ' + defaultIP
  
         plc101 = self.addNode('plc101',ip=IP['plc101'] + NETMASK, cls=LinuxRouter)
         self.addLink( s1, plc101, intfName2='plc101-eth1', params2={ 'ip' : defaultIP } )  
-        self.addLink( s4, plc101, intfName2='plc101-eth2', params2={ 'ip' : '192.168.4.1/24' } )
 
         p101 = self.addHost('p101', ip=IP['p101'] + NETMASK, defaultRoute=gateway_1 )#, defaultRoute="via 192.168.1.11")
         mv101 = self.addHost('mv101',ip=IP['mv101'] + NETMASK, defaultRoute=gateway_1 )#, defaultRoute="via 192.168.1.11")
@@ -56,20 +52,11 @@ class SimpleTopo(Topo):
 
         plc301 = self.addNode('plc301',ip=IP['plc301'] + NETMASK, cls=LinuxRouter)
         self.addLink( s3, plc301, intfName2='plc301-eth1', params2={ 'ip' : '192.168.3.254/24' } )
-        self.addLink( s4, plc301, intfName2='plc301-eth2', params2={ 'ip' : '192.168.4.3/24' } )
 
         lit301 = self.addHost('lit301',ip=IP['lit301'] + NETMASK, defaultRoute=gateway_3)#, defaultRoute="via 192.168.3.10")
         p301 = self.addHost('p301',ip=IP['p301'] + NETMASK, defaultRoute=gateway_3)#, defaultRoute="via 192.168.3.10")
         ids301 = self.addHost('ids301',ip=IP['ids301'] + NETMASK, defaultRoute=gateway_2)#, defaultRoute="via 192.168.2.10")
-        #sim301 = self.addHost('sim301',ip=IP['sim301'] + NETMASK, defaultRoute=gateway_2)#, defaultRoute="via 192.168.2.10")
-        #plant301 = self.addHost('plant301')
-
 
         self.addLink(lit301, s3)        
         self.addLink(p301, s3)                
-        #self.addLink(plc301, s3)      
         self.addLink(ids301, s3)        
-        #self.addLink(sim301, s3)
-
-	self.addLink( s4, ids301, intfName2='ids301-eth3', params2={ 'ip' : '192.168.4.20/24' } )
-
