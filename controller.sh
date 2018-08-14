749dafc25b316eb0ffdec08d0305f08d0faee46d
andrecalderon <andrea_calderon@hotmail.com>
2018-08-14 17:53:18 -0500

    Chaning the way LIT101 sends messages

--- controller.sh
+++
@@ -1,3 +0,0 @@
-#echo "Configuration file: " $PWD/ext/$1
-~/pox/pox.py log.level --WARNING dynamic_controller
-
--- francisco-topo/get_results.sh
+++
@@ -1,3 +0,0 @@
-cat lit101.log | awk '{print $3}' > lit101.dat
-cat lit103.log | awk '{print $3}' > lit103.dat
-
